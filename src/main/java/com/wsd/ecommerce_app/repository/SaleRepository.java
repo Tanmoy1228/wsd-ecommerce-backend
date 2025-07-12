4865e643b83889413d01aae81e70cb5ca1eac3b4
Tanmoy1228 <tanmoy1228@gmail.com>
2025-07-12 22:15:25 +0600

    feat: change SaleRepository class to interface extending JpaRepository and add sql query to pass tests.

--- src/main/java/com/wsd/ecommerce_app/repository/SaleRepository.java
+++ src/main/java/com/wsd/ecommerce_app/repository/SaleRepository.java
@@ -1,16 +1,16 @@
 package com.wsd.ecommerce_app.repository;
 
+import com.wsd.ecommerce_app.model.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Repository
-public class SaleRepository {
+public interface SaleRepository extends JpaRepository<Sale, Long> {
 
-    public BigDecimal findTotalSaleAmountForDateRange(LocalDateTime startOfDay, LocalDateTime endOfDay) {
-        return new BigDecimal("999.99");
-    }
-
-
+    @Query("SELECT COALESCE(SUM(s.amount), 0) FROM Sale s WHERE s.saleDate >= :startOfDay AND s.saleDate <= :endOfDay")
+    BigDecimal findTotalSaleAmountForDateRange(LocalDateTime startOfDay, LocalDateTime endOfDay);
 }
