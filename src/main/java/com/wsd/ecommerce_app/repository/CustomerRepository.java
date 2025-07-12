c4a74fb2c43217c35609ff21a4998e187084ef29
Tanmoy1228 <tanmoy1228@gmail.com>
2025-07-12 17:46:57 +0600

    refactor: cleanup hard coded value from CustomerRepository.

--- src/main/java/com/wsd/ecommerce_app/repository/CustomerRepository.java
+++ src/main/java/com/wsd/ecommerce_app/repository/CustomerRepository.java
@@ -4,12 +4,8 @@ import com.wsd.ecommerce_app.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Set;
-
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-    Set<Long> existingCustomers = Set.of(1L, 2L, 3L);
-
-    boolean existsById(Long customerId);
+    boolean existsById(Long id);
 }
