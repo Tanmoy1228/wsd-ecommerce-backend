7a346c4f82af7b0e1989be01c550df269b3b47cf
Tanmoy1228 <tanmoy1228@gmail.com>
2025-07-12 17:17:00 +0600

    feat: add customers table schema and entity.

---
+++ src/main/java/com/wsd/ecommerce_app/model/Customer.java
@@ -0,0 +1,49 @@
+package com.wsd.ecommerce_app.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", columnDefinition = "VARCHAR(100)", nullable = false)
+    private String name;
+
+    @Column(name = "email", columnDefinition = "VARCHAR(100)", nullable = false, unique = true)
+    private String email;
+
+    public Customer() {}
+
+    public Customer(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
