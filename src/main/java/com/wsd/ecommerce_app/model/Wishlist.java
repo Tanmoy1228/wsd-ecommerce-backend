40fe0f18fe32dfce48fc2236bc597310c3ef0bea
Tanmoy1228 <tanmoy1228@gmail.com>
2025-07-12 18:44:33 +0600

    feat: add wishlist table schema and entity.

---
+++ src/main/java/com/wsd/ecommerce_app/model/Wishlist.java
@@ -0,0 +1,65 @@
+package com.wsd.ecommerce_app.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "wishlists")
+public class Wishlist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(name = "added_date", nullable = false)
+    private LocalDateTime addedDate;
+
+    public Wishlist() {}
+
+    public Wishlist(Customer customer, Product product) {
+        this.customer = customer;
+        this.product = product;
+        this.addedDate = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public LocalDateTime getAddedDate() {
+        return addedDate;
+    }
+
+    public void setAddedDate(LocalDateTime addedDate) {
+        this.addedDate = addedDate;
+    }
+}
