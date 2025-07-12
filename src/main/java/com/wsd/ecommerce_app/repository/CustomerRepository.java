681b306d9a5d54cf8af366d265bd216524e26fd9
Tanmoy1228 <tanmoy1228@gmail.com>
2025-07-12 17:44:53 +0600

    feat: change CustomerRepository class to interface extending JpaRepository to pass tests.

--- src/main/java/com/wsd/ecommerce_app/repository/CustomerRepository.java
+++ src/main/java/com/wsd/ecommerce_app/repository/CustomerRepository.java
@@ -1,15 +1,15 @@
 package com.wsd.ecommerce_app.repository;
 
+import com.wsd.ecommerce_app.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Set;
 
 @Repository
-public class CustomerRepository {
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-    private final Set<Long> existingCustomers = Set.of(1L, 2L, 3L);
+    Set<Long> existingCustomers = Set.of(1L, 2L, 3L);
 
-    public boolean existsById(Long customerId) {
-        return existingCustomers.contains(customerId);
-    }
+    boolean existsById(Long customerId);
 }
