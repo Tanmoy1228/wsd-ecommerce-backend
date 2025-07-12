2956a8424da69a9a2a92be79f7697122ab9bdc0d
Tanmoy1228 <tanmoy1228@gmail.com>
2025-07-12 22:58:56 +0600

    feat: add MaxSaleDayDTO and return hard coded data to pass getMaxSaleDay controller test

--- src/main/java/com/wsd/ecommerce_app/controller/SaleController.java
+++ src/main/java/com/wsd/ecommerce_app/controller/SaleController.java
@@ -1,11 +1,17 @@
 package com.wsd.ecommerce_app.controller;
 
+import com.wsd.ecommerce_app.dto.MaxSaleDayDTO;
 import com.wsd.ecommerce_app.dto.SaleTotalTodayDTO;
 import com.wsd.ecommerce_app.service.SaleService;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/api/sales")
@@ -23,5 +29,18 @@ public class SaleController {
         SaleTotalTodayDTO dto = saleService.getTodaySaleTotal();
 
         return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping("/max-day")
+    public ResponseEntity<MaxSaleDayDTO> getMaxSaleDay(
+            @RequestParam("startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+            @RequestParam("endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
+
+        BigDecimal totalSaleAmount = new BigDecimal("999.99");
+        LocalDate date = LocalDate.of(2024, 7, 5);
+
+        MaxSaleDayDTO maxSaleDayDTO = new MaxSaleDayDTO(date, totalSaleAmount);
+
+        return ResponseEntity.ok(maxSaleDayDTO);
     }
 }
